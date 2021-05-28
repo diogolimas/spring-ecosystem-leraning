78fc3c5533271b1c296aa51ed3ba9f49ee5d1ff4
Diogo Lima <dsilval@indracompany.com>
2021-05-28 16:22:03 -0300

    testes

---
+++ src/test/java/com/indra/treinamento/controller/ProdutoControllerTest.java
@@ -0,0 +1,9 @@
+package com.indra.treinamento.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest(webEnvironment = SpringBooTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureTestDataBase
+public class ProdutoControllerTest {
+
+}
